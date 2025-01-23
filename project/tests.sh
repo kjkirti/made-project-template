6454127eea6fa87e7c7976eaa8616a43cf77fad4
kjkirti <kjkirti111@gmail.com>
2025-01-23 12:48:17 +0100

    changes for yml

--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-# Run pytest
-# commenting out below line as it is giving me failed in project.yml when I upload it 
-#pytest ./project/tests.py
+# Check if Python 3 is installed
+if ! command -v python3 >/dev/null 2>&1; then
+    echo "Pipeline needs Python 3 but it's not installed."
+    exit 1
+fi
 
+# Install required Python packages
+python3 -m pip install -r ./project/requirements.txt
+
+# Ensure pytest is installed
+if ! python3 -m pytest --version >/dev/null 2>&1; then
+    echo "Installing pytest..."
+    python3 -m pip install pytest kaggle
+fi
+
+# Run the ETL pipeline
+echo "Running ETL pipeline..."
+python3 ./project/pipeline.py
+
+# Check if the ETL pipeline ran successfully
+if [ $? -ne 0 ]; then
+    echo "ETL pipeline failed. Skipping tests."
+    exit 1
+fi
+
+# Run the tests
+echo "Running test cases..."
+pytest ./project/tests.py
+
+exit 0
